8d9a9c125505a16329bd7f5399fd6764c74830d1
Mapees <amparo.ep@gmail.com>
2016-10-24 18:27:56 +0200

    Update utils_corpus.py

--- utils_corpus.py
+++ utils_corpus.py
@@ -4,66 +4,61 @@ import re
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 
-tok = TweetTokenizer()
-
-# Leer datos de train.data
+##### LEER DATA #####
 def obtener_data(fichero):
     with open (fichero, 'r') as f:
         data = f.readlines()
         return data
 
-# Obtener tuits y labels
-def obtener_tuits_labels_train(data):
-    tuits = []
+def obtener_train():
+    data = obtener_data('train.data')
+    tweets = []
     labels = []
-    for line in data:
-        sentence = re.split(r'\t+',  line)
-        if ( len(sentence) > 2):		# para evitar lineas vacías
-            tuits.append(sentence[2])
-            label = sentence[-1]
-            label = label[:-1]
-            labels.append(float(label))
-    return tuits, labels
-    
-def obtener_tuits_labels_test(data):
-    tuits = []
-    labels = []
-    for line in data:
-        sentence = re.split(r'\t+',  line)
-        if ( len(sentence) > 2):		# para evitar lineas vacías
-            tuits.append(sentence[2])
-            label = sentence[1]
-            labels.append(float(label))
-    return tuits, labels
-
-
-# Obtener labels
-def obtener_labels_data(data):
-    data_labels = []
     for line in data:
         sentence = re.split(r'\t+',  line)
         if ( len(sentence) > 2):
-            label = sentence[-1]
-            label = label[:-1]
-            data_labels.append(float(label))
-    return data_labels
+            tweets.append(sentence[2])
+            labels.append(float( sentence[-1]))
+    return tweets, labels
+
+def obtener_test():
+    data = obtener_data('test.data')    
+    tweets = []
+    labels = []
+    for line in data:
+        sentence = re.split(r'\t+',  line)
+        if ( len(sentence) > 2):		# para evitar lineas vacias
+            tweets.append(sentence[2])
+            labels.append(float(sentence[1]))
+    return tweets, labels
+
+##### TOKENIZAR ##### 
+emoticons_str = r"""
+    (?:
+        [:=;] 
+        [oO\-]?
+        [D\)\]\(\]/\\OpP]
+        [:D]
+        [:-O]
+    )"""
     
-    
-# Tokenizar el corpus    
-def obtener_corpus_tokenizado(data):
+emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
+
+## Si no pertenece a un emoticono se pasa a minusculas
+def tokenize(data):
     corpus = []
     for line in data:
-        token = tok.tokenize(line)
-        sentence = " ".join(token)
+        tokens = TweetTokenizer().tokenize(line)
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+        sentence = " ".join(tokens)
         corpus.append(sentence)
     return corpus
     
 # Quitar las stopwords de un corpus
-def obtener_corpus_stopwords(data):
+def quitar_stopwords(data):
     corpus =  []
     for line in data:
         token = [word for word in line.split() if word not in stopwords.words('english')]
         sentence = " ".join(token)
         corpus.append(sentence)
     return corpus
-    
